745c3a35dd2bcc7c914c04ecf7cad9c4ba9a70b0
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-11-10 19:51:29 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import Typed from "typed.js";
 import React, { useRef, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image"; // Make sure to import Image from 'next/image'
+import Image from "next/image"; // Import Image from next/image
 
 export default function Home() {
   // Create reference to store the DOM element containing the animation
@@ -41,14 +41,17 @@ export default function Home() {
             Crafting the Future of Innovation
             <br className="hidden lg:block" /> Inspiring Tomorrow’s Innovators
           </p>
+          <div className="mt-6 bg-transparent border rounded-lg dark:border-gray-700 lg:w-2/3 focus-within:border-primary focus-within:ring focus-within:ring-primary dark:focus-within:border-primary focus-within:ring-opacity-20">
+            <form action="https://www.creative-tim.com/twcomponents/search" className="flex flex-wrap justify-between md:flex-row"></form>
+          </div>
         </div>
         <div className="w-full mt-4 lg:mt-0 lg:w-1/2">
           <Image
             src="https://www.creative-tim.com/twcomponents/svg/website-designer-bro-purple.svg"
             alt="tailwind css components"
             className="w-full h-full max-w-md mx-auto"
-            width={500} // You can specify the width here
-            height={500} // You can specify the height here
+            width={500} // Add width for optimization
+            height={250} // Add height for optimization
           />
         </div>
       </section>
@@ -64,9 +67,7 @@ export default function Home() {
             <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
               <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105 text-center">
                 <p className="text-gray-600 dark:text-gray-400">
-                  Om is the visionary who leads Python Pathways with clarity and purpose. His relentless pursuit of
-                  excellence and innovation sets the tone for the club’s growth, ensuring every member is empowered to
-                  push boundaries and achieve greatness.
+                  Om is the visionary who leads Python Pathways with clarity and purpose. His relentless pursuit of excellence and innovation sets the tone for the club’s growth, ensuring every member is empowered to push boundaries and achieve greatness.
                 </p>
                 <h3 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-200">Om Rajguru</h3>
                 <p className="text-gray-500 dark:text-gray-300">President</p>
@@ -76,9 +77,7 @@ export default function Home() {
             <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
               <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105 text-center">
                 <p className="text-gray-600 dark:text-gray-400">
-                  Saif is the architect of our operations, bringing strategic focus and meticulous execution to every
-                  project. As Viceroy and President of Production, he ensures that our ideas come to life seamlessly,
-                  delivering top-tier results with every initiative.
+                  Saif is the architect of our operations, bringing strategic focus and meticulous execution to every project. As Viceroy and President of Production, he ensures that our ideas come to life seamlessly, delivering top-tier results with every initiative.
                 </p>
                 <h3 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-200">Saif Mulla</h3>
                 <p className="text-gray-500 dark:text-gray-300">Viceroy & President of Production</p>
@@ -88,9 +87,7 @@ export default function Home() {
             <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
               <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105 text-center">
                 <p className="text-gray-600 dark:text-gray-400">
-                  Zoyan is the driving force behind collaboration and creative synergy within Python Pathways. His
-                  leadership nurtures an environment where talent flourishes, guiding the team with vision and passion to
-                  shape the future of tech.
+                  Zoyan is the driving force behind collaboration and creative synergy within Python Pathways. His leadership nurtures an environment where talent flourishes, guiding the team with vision and passion to shape the future of tech.
                 </p>
                 <h3 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-200">Zoyan Namazi</h3>
                 <p className="text-gray-500 dark:text-gray-300">President</p>
@@ -112,13 +109,13 @@ export default function Home() {
               <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105">
                 <Image
                   src="/typescript.webp"
-                  alt="Blog 1"
-                  width={500} // Specify width for Image
-                  height={500} // Specify height for Image
+                  alt="A Journey to Computing's Next Frontier"
                   className="w-full h-64 object-cover rounded-t-lg"
+                  width={500} // Add width for optimization
+                  height={250} // Add height for optimization
                 />
                 <div className="mt-4">
-                  <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">A Journey to Computing's Next Frontier</h3>
+                  <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">A Journey to Computing&apos;s Next Frontier</h3>
                   <p className="mt-2 text-gray-600 dark:text-gray-400">An insight-filled journey awaits. Check back soon to explore and inspire.</p>
                   <Link href="/blogpost/a-journey-to-computings-next-frontier" passHref>
                     <Button className="m-2" variant="outline" as="a">
@@ -134,13 +131,15 @@ export default function Home() {
                 <Image
                   src="https://images.pexels.com/photos/1181472/pexels-photo-1181472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="Blog 2"
-                  width={500} // Specify width for Image
-                  height={500} // Specify height for Image
                   className="w-full h-64 object-cover rounded-t-lg"
+                  width={500} // Add width for optimization
+                  height={250} // Add height for optimization
                 />
                 <div className="mt-4">
                   <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">In the Works</h3>
-                  <p className="mt-2 text-gray-600 dark:text-gray-400">We’re crafting something exclusive. Prepare for a deeper look into the future of tech and innovation.</p>
+                  <p className="mt-2 text-gray-600 dark:text-gray-400">
+                    We&apos;re crafting something exclusive. Prepare for a deeper look into the future of tech and innovation.
+                  </p>
                   <Link href="/blogpost/in-the-works" passHref>
                     <Button className="m-2" variant="outline" as="a">
                       Read More
@@ -155,14 +154,14 @@ export default function Home() {
                 <Image
                   src="https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg"
                   alt="Blog 3"
-                  width={500} // Specify width for Image
-                  height={500} // Specify height for Image
                   className="w-full h-64 object-cover rounded-t-lg"
+                  width={500} // Add width for optimization
+                  height={250} // Add height for optimization
                 />
                 <div className="mt-4">
-                  <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Building A Legacy</h3>
-                  <p className="mt-2 text-gray-600 dark:text-gray-400">The vision to build something great doesn’t stop at a project. Learn more about our legacy of innovation.</p>
-                  <Link href="/blogpost/building-a-legacy" passHref>
+                  <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">On the Horizon</h3>
+                  <p className="mt-2 text-gray-600 dark:text-gray-400">An insight-filled journey awaits. Check back soon to explore and inspire.</p>
+                  <Link href="/blogpost/on-the-horizon" passHref>
                     <Button className="m-2" variant="outline" as="a">
                       Read More
                     </Button>
