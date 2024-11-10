5981055930747255bce6eee1d769c63897bf3af5
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-11-10 22:20:15 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Python Pathways Blogs",
-  description: "Python Pathways Blogs",
+  description: "Python Pathways Blogs ",
 };
 
 export default function RootLayout({ children }) {
