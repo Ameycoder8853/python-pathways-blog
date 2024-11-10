7bbc64603b38e6461a6acc9c2c479e7b7cbfc9a8
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-11-10 22:31:51 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -85,7 +85,7 @@ const Navbar = () => {
                         </SheetTrigger>
                         <SheetContent>
                             <SheetHeader>
-                                <SheetTitle className="font-bold my-4">AmeyBlog</SheetTitle>
+                                <SheetTitle className="font-bold my-4">Python Pathways</SheetTitle>
                                 <SheetDescription>
                                     <div className="flex flex-col gap-6">
                                         <Link href="/"> Home
