6b65cd3156f666aec27fdcabfaa65da6c546c264
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-11-10 20:19:05 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@
 import { Button } from "@/components/ui/button";
 import Typed from 'typed.js';
 import React, { useRef, useEffect } from 'react';
+import Image from 'next/image'; // Import Image from Next.js
 import Link from "next/link";
-import Image from 'next/image'; // Import Image component
 
 export default function Home() {
   // Create reference to store the DOM element containing the animation
@@ -11,7 +11,13 @@ export default function Home() {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['Transforming Passion into Proficiency.', 'Exploring Boundaries, Empowering Coders.', ' Where Curiosity Meets Innovation.', 'Crafting Tomorrows Tech Leaders.', 'Unlocking the Power of Python, Together.'],
+      strings: [
+        'Transforming Passion into Proficiency.',
+        'Exploring Boundaries, Empowering Coders.',
+        'Where Curiosity Meets Innovation.',
+        'Crafting Tomorrow\'s Tech Leaders.',
+        'Unlocking the Power of Python, Together.'
+      ],
       typeSpeed: 50,
     });
 
@@ -29,20 +35,20 @@ export default function Home() {
             Welcome <span className="font-semibold">to</span> Python Pathways <br className="hidden lg:block" /> Blogs <span className="font-semibold underline decoration-primary"><span ref={el} /></span>
           </h1>
           <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
-            Crafting the Future of Innovation<br className="hidden lg:block" /> Inspiring Tomorrow&apos;s Innovators
+            Crafting the Future of Innovation<br className="hidden lg:block" /> Inspiring Tomorrow’s Innovators
           </p>
           <div className="mt-6 bg-transparent border rounded-lg dark:border-gray-700 lg:w-2/3 focus-within:border-primary focus-within:ring focus-within:ring-primary dark:focus-within:border-primary focus-within:ring-opacity-20">
             <form action="https://www.creative-tim.com/twcomponents/search" className="flex flex-wrap justify-between md:flex-row">
-
             </form>
           </div>
         </div>
         <div className="w-full mt-4 lg:mt-0 lg:w-1/2">
           <Image 
             src="https://www.creative-tim.com/twcomponents/svg/website-designer-bro-purple.svg" 
-            alt="Website Designer" 
+            alt="tailwind css components" 
             width={600} 
-            height={600} 
+            height={400} 
+            className="w-full h-full max-w-md mx-auto" 
           />
         </div>
       </section>
@@ -57,7 +63,7 @@ export default function Home() {
             {/* Testimonial 1 */}
             <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
               <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105 text-center">
-                <p className="text-gray-600 dark:text-gray-400">Om is the visionary who leads Python Pathways with clarity and purpose. His relentless pursuit of excellence and innovation sets the tone for the club&apos;s growth, ensuring every member is empowered to push boundaries and achieve greatness.</p>
+                <p className="text-gray-600 dark:text-gray-400">Om is the visionary who leads Python Pathways with clarity and purpose. His relentless pursuit of excellence and innovation sets the tone for the club’s growth, ensuring every member is empowered to push boundaries and achieve greatness.</p>
                 <h3 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-200">Om Rajguru</h3>
                 <p className="text-gray-500 dark:text-gray-300">President</p>
               </div>
@@ -94,19 +100,19 @@ export default function Home() {
               <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105">
                 <Image 
                   src="/typescript.webp" 
-                  alt="A Journey to Computing's Next Frontier" 
+                  alt="Blog 1" 
                   width={600} 
                   height={400} 
                   className="w-full h-64 object-cover rounded-t-lg" 
                 />
                 <div className="mt-4">
-                  <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">A Journey to Computing&apos;s Next Frontier</h3>
+                  <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">A Journey to Computing's Next Frontier</h3>
                   <p className="mt-2 text-gray-600 dark:text-gray-400">An insight-filled journey awaits. Check back soon to explore and inspire.</p>
                   <Link href="/blogpost/a-journey-to-computings-next-frontier" passHref>
                     <Button className="m-2" variant="outline" as="a">
                       Read More
                     </Button>
-                  </Link>       
+                  </Link>
                 </div>
               </div>
             </div>
@@ -115,40 +121,40 @@ export default function Home() {
               <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105">
                 <Image 
                   src="https://images.pexels.com/photos/1181472/pexels-photo-1181472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-                  alt="Blog Image" 
+                  alt="Blog 2" 
                   width={600} 
                   height={400} 
                   className="w-full h-64 object-cover rounded-t-lg" 
                 />
                 <div className="mt-4">
                   <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">In the Works</h3>
-                  <p className="mt-2 text-gray-600 dark:text-gray-400"> We&apos;re crafting something exclusive. Prepare for a deeper look into the future of tech and innovation.</p>
+                  <p className="mt-2 text-gray-600 dark:text-gray-400"> We’re crafting something exclusive. Prepare for a deeper look into the future of tech and innovation.</p>
                   <Link href="/blogpost/in-the-works" passHref>
                     <Button className="m-2" variant="outline" as="a">
                       Read More
                     </Button>
-                  </Link>                
+                  </Link>
                 </div>
               </div>
             </div>
-            {/* Blog 3 */}
+            {/* Blog 3 - On the Horizon */}
             <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
               <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105">
                 <Image 
-                  src="https://images.pexels.com/photos/1181236/pexels-photo-1181236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-                  alt="Blog Image" 
+                  src="https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg" 
+                  alt="On the Horizon" 
                   width={600} 
                   height={400} 
                   className="w-full h-64 object-cover rounded-t-lg" 
                 />
                 <div className="mt-4">
                   <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">On the Horizon</h3>
-                  <p className="mt-2 text-gray-600 dark:text-gray-400">Big things are coming your way. Stay tuned for updates on groundbreaking new tech.</p>
+                  <p className="mt-2 text-gray-600 dark:text-gray-400">Discover what’s coming next. Our upcoming blog post promises to push the boundaries.</p>
                   <Link href="/blogpost/on-the-horizon" passHref>
                     <Button className="m-2" variant="outline" as="a">
                       Read More
                     </Button>
-                  </Link>               
+                  </Link>
                 </div>
               </div>
             </div>
